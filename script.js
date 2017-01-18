77d1a8ef7dae8dc8bbf0e8fae11faad7b5699833
stacy <smartinez01.student@hsfi.us>
2017-01-18 21:59:59 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,39 @@
+TinyTurtle.apply(window); 
+  forward(40);
+  right(70);
+  forward(40);
+  right(40);
+  forward(40);
+  right (70);
+  forward (40);
+  left(270); //same as left (-90);  or right(90);
+  forward(75); 
+   penUp();
+ forward(20);
+ penDown();
+  
+  right(20);
+  forward (20);
+  left(50);
+  forward(20);
+  left(120);
+  forward(50);
+  right(110);
+  forward (20);
+  right(60);
+  forward(30);
+  
+  
+   penUp();
+ forward(10);
+ penDown();
+ right(-110);
+ forward(40);
+ left(180);
+ forward(40);
+ left(120);
+ forward(40);
+ right(60);
+ forward(40);
+ right (-110);
+ forward (40);
